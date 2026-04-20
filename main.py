46f2156ef7d1bd73c751d097df6ad8bd2906422f
Kin <kin.ma.027@gmail.com>
2026-04-20 11:21:30 -0500

    Add code to read T-100 dataset and filter DB1B down to routes without a nonstop flight (yay!)

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ origin_airports_list = origin_airports_series.to_list()
 
 # Perform input validation based on this list
 if origin_airport_upper in origin_airports_list:
-    # Create new df that filters down to rows where origin airport is user input
+    # Filter DB1B_df down to rows where origin airport is same as user input
     filtered_DB1B_df = DB1B_df[DB1B_df.ORIGIN == origin_airport_upper]
 
     # Create new column that multiplies passenger counts by 10, as DB1B is a 10% sample of tickets
@@ -26,8 +26,25 @@ if origin_airport_upper in origin_airports_list:
     # Sort the df by passenger counts, highest to lowest, using merge sort
     filtered_DB1B_df.sort_values(by = "passengers_times_10", ascending = False, inplace = True, kind = "mergesort")
 
-    # Print df (for now)
-    print(filtered_DB1B_df)
+    # Read T-100 CSV file with pandas
+    T100_df = pd.read_csv("final_datasets/T100_2024.csv")
+
+    # Filter T100_df down to rows where origin airport is same as user input and passengers > 0 (as T100 contains cargo
+    # flights)
+    filtered_T100_df = T100_df[(T100_df.ORIGIN == origin_airport_upper) & (T100_df.PASSENGERS > 0)]
+
+    # Create a new series of boolean values that indicate whether a route has a nonstop flight by cross-checking with
+    # T100_df, which indicates whether a nonstop flight exists
+    has_nonstop_series = filtered_DB1B_df["DEST"].isin(filtered_T100_df["DEST"])
+
+    # Create new column in filtered_DB1B_df that pastes this series
+    filtered_DB1B_df["has_nonstop_flight"] = has_nonstop_series
+
+    # Filter filtered_DB1B_df down to rows where "has_nonstop_flight" is false
+    filtered_DB1B_df_2 = filtered_DB1B_df[filtered_DB1B_df["has_nonstop_flight"] == False]
+
+    # Print DB1B_df (for now)
+    print(filtered_DB1B_df_2)
 
 else: # Invalid airport
     # Print error message
