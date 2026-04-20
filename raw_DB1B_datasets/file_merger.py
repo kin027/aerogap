bbd2767ec7fcfb4cb6fd24bf86eec71def69569b
Kin <kin.ma.027@gmail.com>
2026-04-19 21:11:06 -0500

    Add more comments to explain more steps

--- raw_DB1B_datasets/file_merger.py
+++ raw_DB1B_datasets/file_merger.py
@@ -1,4 +1,4 @@
-# This file combines the four quarterly DB1B datasets and aggregates passenger counts
+# This program combines the four quarterly DB1B datasets and aggregates passenger counts, but does NOT multiply the passenger counts by 10
 
 import pandas as pd
 import glob
@@ -15,22 +15,30 @@ print(f"Found {len(file_list)} files to process")
 
 summaries = []
 
+# For loop for each quarter DB1B file
 for file in file_list:
     print(f"Processing {os.path.basename(file)}...")
 
+    # Convert to a DataFrame
     df = pd.read_csv(file)
 
-    # Aggregate passenger count of a quarter DB1B file
+    # Sum up passenger counts for a unique route
     quarter_summary = df.groupby(['ORIGIN', 'DEST'])['PASSENGERS'].sum().reset_index()
+
+    # Append to summaries array
     summaries.append(quarter_summary)
 
 # Combine and save to the 'datasets' folder (one level up from this script)
 if summaries:
+    # Concatenate each df in the summaries array
     annual_data = pd.concat(summaries)
+
+    # Sum up passenger counts for a unique route again due duplication of routes between quarters
     final_total = annual_data.groupby(['ORIGIN', 'DEST'])['PASSENGERS'].sum().reset_index()
 
-    # Join path to go up one level, then into 'datasets'
+    # Set the output path for the final csv
     output_path = os.path.join(script_dir, "..", "datasets", "DB1B_2024_consolidated.csv")
 
+    # Convert the final_total df to the final csv
     final_total.to_csv(output_path, index=False)
     print(f"Final file saved at {output_path}")
