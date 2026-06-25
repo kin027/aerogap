837ff4d097acbcbbab020041a5439578aa1d3ad1
Kin <kin.ma.027@gmail.com>
2026-06-25 16:58:33 -0600

    rename files and folders

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from unserved_flight_routes_analyzer import UnservedFlightRoutesAnalyzer
 
 # Set global constants for paths of files
-DB1B_PATH = "final_datasets/DB1B_2024_consolidated.csv"
-T100_PATH = "final_datasets/T100_2024.csv"
+DB1B_PATH = "final_tables/DB1B_2024_consolidated.csv"
+T100_PATH = "final_tables/T100_2024.csv"
 
 def main():
     # Create UnservedFlightRoutesAnalyzer object
