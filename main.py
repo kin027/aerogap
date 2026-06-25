5e83f86b09c8c600112dcc5c8a7bf3fdc090b411
Kin <kin.ma.027@gmail.com>
2026-06-25 12:47:19 -0600

    Added run function, fixed blank window appearing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from unserved_routes_analyzer import UnservedRoutesAnalyzer
+from unserved_flight_routes_analyzer import UnservedFlightRoutesAnalyzer
 
 # Set global constants for paths of files
 DB1B_PATH = "final_datasets/DB1B_2024_consolidated.csv"
@@ -6,9 +6,9 @@ T100_PATH = "final_datasets/T100_2024.csv"
 
 def main():
     # Create RoutesAnalyzer object
-    analyzer = UnservedRoutesAnalyzer(DB1B_PATH, T100_PATH)
+    analyzer = UnservedFlightRoutesAnalyzer(DB1B_PATH, T100_PATH)
 
     # Call analyzer analyze_routes method
-    analyzer.get_origin_airport()
+    analyzer.run()
 
 main()
--- unserved_flight_routes_analyzer.py
+++ unserved_flight_routes_analyzer.py
@@ -1,13 +1,14 @@
 # Class for analyzing
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import tkinter
-from tkinter import simpledialog
-from tkinter import messagebox
+from tkinter import simpledialog, messagebox
 
 TITLE = "Popular Unserved Flight Routes"
 
-class UnservedRoutesAnalyzer:
+class UnservedFlightRoutesAnalyzer:
     def __init__(self, db1b_path, t100_path):
         # Read both DB1B and T-100 CSVs first
         self.DB1B_df = pd.read_csv(db1b_path)
@@ -16,46 +17,44 @@ class UnservedRoutesAnalyzer:
         # Get a set of valid origin airport codes
         self.origin_airports_set = set(self.DB1B_df.ORIGIN)
 
+        # Create new Tkinter window then withdraw it
+        self.window = tkinter.Tk()
+        self.window.withdraw()
+
+        # Set attributes for original airport and final dataframe
+        self.origin_airport = None
+        self.final_df = None
+
     # Method to get airport code from user
     def get_origin_airport(self):
-        # Create new Tkinter window then withdraw it
-        window = tkinter.Tk()
-        window.withdraw()
-
         # Ask user for airport code
         origin_airport = simpledialog.askstring(title = TITLE, prompt = "Enter a three-character IATA airport code:")
 
-        if origin_airport is not None:
+        if origin_airport:
             # Convert user input to uppercase
             origin_airport = origin_airport.upper()
 
-            # Call method to validate origin airport
-            self.check_valid_origin_airport(origin_airport)
-        else: # User hit the cancel button
-            # End program
-            return
+            # Set origin_airport attribute to result
+            self.origin_airport = origin_airport
 
     # Method to validate user-entered origin airport
-    def check_valid_origin_airport(self, origin_airport):
-        if origin_airport in self.origin_airports_set: # Valid airport
-            # Call method to analyze data tables
-            self.analyze_unserved_routes(origin_airport)
+    def validate_origin_airport(self):
+        if self.origin_airport in self.origin_airports_set: # Valid airport
+            return True
         else: # Invalid airport
             # Display message box for error message
-            messagebox.showerror(message = "Airport nonexistent, does not have scheduled commercial air service, or is "
-                                           "not in the U.S.", title = TITLE)
-
-            # End the program
-            return
+            messagebox.showerror(message = "Airport is nonexistent, does not have scheduled commercial air service, or "
+                                           "is not in the U.S.", title = TITLE)
+            return False
 
     # Method to analyze data tables
-    def analyze_unserved_routes(self, origin_airport):
+    def analyze_data_tables(self):
         # Clean datasets by dropping N/A values (shouldn't be there anyway but double-check)
         self.DB1B_df.dropna(inplace = True)
         self.T100_df.dropna(inplace = True)
 
         # Filter DB1B_df down to rows where origin airport is same as user input
-        filtered_DB1B_df = self.DB1B_df[self.DB1B_df.ORIGIN == origin_airport].copy()
+        filtered_DB1B_df = self.DB1B_df[self.DB1B_df.ORIGIN == self.origin_airport].copy()
 
         # Create new column that multiplies passenger counts by 10, as DB1B is a 10% sample of tickets
         filtered_DB1B_df["PASSENGERS_TIMES_10"] = filtered_DB1B_df["PASSENGERS"] * 10
@@ -69,7 +68,7 @@ class UnservedRoutesAnalyzer:
 
         # Filter T100_df down to rows where origin airport is same as user input and passengers > 0 (as T100 contains
         # cargo flights)
-        filtered_T100_df = self.T100_df[(self.T100_df.ORIGIN == origin_airport) & (self.T100_df.PASSENGERS > 0)]
+        filtered_T100_df = self.T100_df[(self.T100_df.ORIGIN == self.origin_airport) & (self.T100_df.PASSENGERS > 0)]
 
         # Create a new column of boolean values in filtered_DB1B_df that indicates whether a route has a nonstop
         # flight by cross-checking with T100_df, which indicates whether a nonstop flight exists
@@ -83,23 +82,27 @@ class UnservedRoutesAnalyzer:
 
         # Deal with edge case where filtered df might be empty
         if not filtered_DB1B_df.empty:
-            # Call create_bar_graph method
-            self.create_bar_graph(filtered_DB1B_df, origin_airport)
+            self.final_df = filtered_DB1B_df
+            return True
         else:
-            messagebox.showerror(message=f"Everyone who flew out of {origin_airport} took a nonstop flight. (This is common with airports served exclusively by a budget airline that does not sell connecting itineraries.)", title=TITLE)
+            messagebox.showerror(message=f"Everyone who flew out of {self.origin_airport} took a nonstop flight. (This "
+                                         f"is common with airports served exclusively by a budget airline that does "
+                                         f"not sell connecting itineraries.)", title=TITLE)
+            return False
 
-    def create_bar_graph(self, df, origin_airport):
+    # Method to create and show the graph
+    def create_bar_graph(self):
         # Format graph
         fig, ax = plt.subplots(figsize = (12, 8))
 
         # Title
-        ax.text(0, 1.18, f"Most popular unserved flight routes from {origin_airport} in 2024",
+        ax.text(0, 1.22, f"Most popular unserved flight routes from {self.origin_airport} in 2024",
                 transform = ax.transAxes, fontsize = 24, va = 'top')
 
         # Subtitle
         ax.text(0, 1.04, "Based on Bureau of Transportation Statistics (BTS) 2024 DB1B tables.\nIn parentheses under"
-                         " each count is the average daily passenger count. A Boeing 737-800 seats around 160 "
-                         "passengers.",
+                         " each count is the average daily passenger count.\nFor context, a Boeing 737-800 seats "
+                         "around 160 passengers, and an Embraer E175LR seats around 76 passengers.",
                 transform = ax.transAxes, fontsize = 12, color = '#a7a9ac', va = 'bottom')
 
         # Labels and scale
@@ -107,22 +110,47 @@ class UnservedRoutesAnalyzer:
         plt.ylabel("Passengers", labelpad = 20, fontsize=18)
         plt.xticks(fontsize = 12)
         plt.yticks(fontsize = 12)
-        plt.ylim(0, df["PASSENGERS_TIMES_10"].max() * 1.1)
+        plt.ylim(0, self.final_df["PASSENGERS_TIMES_10"].max() * 1.1)
 
         # Remove top and right borders
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
 
         # Change window title
-        fig.canvas.manager.set_window_title(TITLE)
+        fig.canvas.manager.set_window_title(f"Most popular unserved flight routes from {self.origin_airport} in 2024")
 
         # Create graph
-        graph = plt.bar(df.DEST, df.PASSENGERS_TIMES_10, color='#0039a6')
-        bar_labels = df["PASSENGERS_TIMES_10"].astype(str).str.cat("\n(" + df["PASSENGERS_DAILY"].astype(str) + ")")
-        plt.bar_label(graph, df.PASSENGERS_TIMES_10.map(int).astype(str) + "\n(" +
-                      df.PASSENGERS_DAILY.map(float).astype(str) + ")", label_type="center", padding=2,
+        graph = plt.bar(self.final_df.DEST, self.final_df.PASSENGERS_TIMES_10, color='#0039a6')
+        bar_labels = self.final_df["PASSENGERS_TIMES_10"].astype(str).str.cat("\n(" +
+                     self.final_df["PASSENGERS_DAILY"].astype(str) + ")")
+        plt.bar_label(graph, self.final_df.PASSENGERS_TIMES_10.map(int).astype(str) + "\n(" +
+                      self.final_df.PASSENGERS_DAILY.map(float).astype(str) + ")", label_type="center", padding=2,
                       color="w", fontsize=12)
 
         # Show graph
         plt.tight_layout()
         plt.show()
+
+    def run(self):
+        # Call method to get airport code from user
+        self.get_origin_airport()
+
+        if not self.origin_airport: # User hit the cancel button
+            self.window.destroy()
+            return
+
+        # Call method to validate user-entered origin airport
+        if not self.validate_origin_airport():
+            self.window.destroy()
+            return
+
+        # Call method to analyze data tables
+        if not self.analyze_data_tables():
+            self.window.destroy()
+            return
+
+        # Call the method to create and show the graph
+        self.create_bar_graph()
+
+        # Destroy the window
+        self.window.destroy()
