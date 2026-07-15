67bc2d74faeae68b363b7b29928227983b4b36b9
Kin <kin.ma.027@gmail.com>
2026-07-14 22:14:12 -0600

    Update comments on DB1C_merger

--- DB1C_tables/DB1C_merger.py
+++ DB1C_tables/DB1C_merger.py
@@ -2,21 +2,21 @@ import pandas as pd
 import glob
 import os
 
-# Get the absolute path of the folder where this script lives
+# Get absolute path of the folder where this script lives
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Find files in that same folder
 file_pattern = os.path.join(script_dir, "*.parquet")
 file_list = glob.glob(file_pattern)
 
-# Fill list
+# Fill db1c_list
 target_column_list = ["SchFlYear", "SchFlMonth", "Origin", "Dest", "Passengers"]
 db1c_list = [pd.read_parquet(file, columns=target_column_list) for file in file_list]
 
-# Concatenate each df in the db1c_list array
+# Concatenate each df in db1c_list
 db1c_df = pd.concat(db1c_list)
 
-# Sum up passenger counts for a unique route again due duplication of routes within and between months
+# Sum up passenger counts for a unique route
 db1c_df = (
     db1c_df.groupby(["SchFlYear", "SchFlMonth", "Origin", "Dest"])["Passengers"]
     .sum()
@@ -27,8 +27,6 @@ db1c_df = (
 db1c_df[["SchFlYear", "SchFlMonth", "Passengers"]] = db1c_df[
     ["SchFlYear", "SchFlMonth", "Passengers"]
 ].astype("int64")
-
-print(db1c_df.dtypes)
 
 # Set the output path for the final csv
 output_path = os.path.join(script_dir, "..", "final_db1c.csv")
